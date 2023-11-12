bfe615ebf28097080264e6495e95d8807d74fa1f
trkielpaulolayug <88868806+KielPaulo@users.noreply.github.com>
2023-11-12 21:37:02 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [text, setText] = useState('');
+
+  const handleTextAreaChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleLogButtonClick = () => {
+    console.log(text);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Send me anonymous message</h1>
+      <textarea rows="4" cols="50" placeholder="Enter message here..." value={text} onChange={handleTextAreaChange} />
+      <br />
+      <button onClick={handleLogButtonClick}>Send</button>
     </div>
   );
 }
