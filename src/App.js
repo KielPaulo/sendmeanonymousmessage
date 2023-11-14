814fff5b0cfe21da17578d5bd2bb38314f0b30f5
trkielpaulolayug <88868806+KielPaulo@users.noreply.github.com>
2023-11-14 16:03:25 +0800

    Initial CSS styling

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ function App() {
     setSubmitting(true);
 
     try {
-      const response = await axios.post(API_URL, { emailContent });
-      alert(response.data); // Log success or error
+      if (!emailContent.trim()) {
+        alert('Please write a message.');
+      } else {
+        const response = await axios.post(API_URL, { emailContent });
+        alert(response.data);
+      }
     } catch (error) {
       console.error(`Error connecting to ${API_URL}`);
       alert('Error:', error);
@@ -22,18 +26,24 @@ function App() {
   };
 
   return (
-    <div className="p-8">
-      <h1>Send Pao an anonymous message</h1>
-      <form onSubmit={handleSubmit}>
-        <textarea rows="4" cols="50" value={emailContent} onChange={(e) => setEmailContent(e.target.value)} />
+    <div className="p-8 min-h-screen flex justify-center items-center flex-col">
+      <h1 className="pb-8 text-xl">Send Pao an anonymous message</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center">
+        <textarea
+          className="border rounded p-2 focus:outline-none focus:ring focus:border-blue-300 w-full md:w-96"
+          rows="4"
+          cols="50"
+          value={emailContent}
+          onChange={(e) => setEmailContent(e.target.value)}
+        />
         <br />
-        <button type="submit" disabled={submitting}>
+        <button className="btn btn-primary w-full" type="submit" disabled={submitting}>
           {submitting ? 'Sending...' : 'Send'}
         </button>
       </form>
       <br />
       <p>
-        Medyo mabagal mag send, free hosting kasi. And pangit pa wala pa design, basta gumagana muna messaging function saka ko na lang
+        Medyo mabagal mag-send, free hosting kasi. And pangit pa wala pa design, basta gumagana muna messaging function saka ko na lang
         improve to bwahahahaha. Please do not spam din pala.
       </p>
     </div>
